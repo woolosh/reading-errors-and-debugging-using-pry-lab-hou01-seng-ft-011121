844224a9f9e9b62475e3f996d75f612ca3f59b80
Joshua Morales <joshuapmorales@gmail.com>
2020-12-22 02:48:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,10 +13,9 @@ end
 
 def selection(num)
   if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-     binding.pry
+    puts "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+    puts  "HAM HAM HAM IN MY TUMMY"
   end
 end
 
